60f14cdbeb5aa85aedf199e034731bed29c63661
Gimbatov <thegimbatov@yandex.ru>
2024-12-14 14:51:25 +0300

    добавил новые разделы в настройки и оформил страницу настроек

--- lib/navigation_pages/setting_nav_page.dart
+++ lib/navigation_pages/setting_nav_page.dart
@@ -36,11 +36,88 @@ class _SettingNavPageState extends State<SettingNavPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        child: Text('Выйти'),
-        onPressed: isOut ? null : _signOut,
-      ),
+    return Column(
+      children: [
+        Expanded(
+          flex: 9,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          color: appHomeTheme.primaryColor,
+                          width: double.infinity,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            formSpacer,
+                            CircleAvatar(
+                              child: Icon(Icons.person, size: 35,),
+                              maxRadius: 55,
+                            ),
+                            formSpacer,
+                            Text('Имя пользователя', style: TextStyle(fontSize: 20, color: formColor),)
+                          ],
+                        ),
+                      ]
+                    )
+                ),
+                Expanded(
+                  flex: 7,
+                    child: Padding(
+                      padding: formPadding,
+                      child: ListView(
+                        children: [
+                          ListTile(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              side: BorderSide(
+                                color: appHomeTheme.primaryColor
+                              )
+                            ),
+                            title: Text('Личные данные', style: TextStyle(fontSize: 20),),
+                          ),
+                          formSpacer,
+                          ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                side: BorderSide(
+                                    color: appHomeTheme.primaryColor
+                                )
+                            ),
+                            title: Text('Категории', style: TextStyle(fontSize: 20),),
+                          ),
+                          formSpacer,
+                          ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                side: BorderSide(
+                                    color: appHomeTheme.primaryColor
+                                )
+                            ),
+                            title: Text('Настройки', style: TextStyle(fontSize: 20),),
+                          ),
+                        ],
+                      ),
+                    )
+                )
+              ],
+            )
+        ),
+        Expanded(
+          flex: 1,
+          child: Center(
+            child: TextButton(
+              child: Text('Выйти', style: TextStyle(color: Colors.red, fontSize: 20),),
+              onPressed: isOut ? null : _signOut,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
